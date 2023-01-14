642b511494a2d344ebb1ae0d42995671bbfdb1d2
Fedejalife99 <fedejalife@gmail.com>
2023-01-14 07:45:27 -0800

    first commit task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_full - function that verify if the tree is full
+ *@tree: root of the tree
+ *Return: 0 if its not full and 1 if it is
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{	
+	if (tree == NULL)
+		return (0);
+	if (tree->right && tree->left)
+	{
+		binary_tree_is_full(tree->right);
+		binary_tree_is_full(tree->left);
+	}
+	if (tree->right != NULL && tree->left != NULL)
+		return (1);
+	return (0);
+}
