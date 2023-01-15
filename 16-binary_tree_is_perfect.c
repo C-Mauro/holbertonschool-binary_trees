05d0a9778d4c6246f99c1285251f96429c558446
C-Mauro <maurocamila744@gmail.com>
2023-01-15 15:39:58 -0800

    fix file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,7 +10,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int balance, full;
 
 	if (!tree)
-		return (0);
+		return (0); 
 
 	if (tree)
 	{
@@ -18,9 +18,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		full = binary_tree_is_full(tree);
 		if (balance == 0 && full == 1)
 		{
-			printf("balance: %d full: %d\n", balance, full);
 			return (1);
-
+	
 		}
 	}
 	return (0);
