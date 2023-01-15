1923833224b7e24aefa987be0f7908f7d7cc278e
C-Mauro <maurocamila744@gmail.com>
2023-01-15 09:20:50 -0800

    fix betty documentation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,28 +7,35 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
-	
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int height = 0;
 
+	if (!tree)
+		height = 0
+		return (0);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 }
 
+/**
+ *binary_tree_height - function that calculates the height of a binary tree
+ *@tree: root of the tree
+ *Return: height of the tree or 0 if root is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t right = 0, left = 0;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-
-        if (tree)
-	{
+	if (tree->right != NULL)
 		right = 1 + binary_tree_height(tree->right);
-                left = 1 + binary_tree_height(tree->left);
-	}
 
-        if (left > right)
-                return (left);
-        return (right);
+	if (tree->left != NULL)
+		left = 1 + binary_tree_height(tree->left);
+
+	if (left > right)
+		return (left);
+	return (right);
 }
