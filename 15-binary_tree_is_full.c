1b209547f4a74464038bb2e93c32b09784bfe7ba
Fedejalife99 <fedejalife@gmail.com>
2023-01-15 05:47:47 -0800

    task 15 second commit

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,15 +7,12 @@
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
-{	
-	if (tree == NULL)
-		return (0);
-	if (tree->right && tree->left)
-	{
-		binary_tree_is_full(tree->right);
-		binary_tree_is_full(tree->left);
-	}
-	if (tree->right != NULL && tree->left != NULL)
-		return (1);
-	return (0);
+{
+        if (!tree)
+                return (0);
+
+        if (tree->right == NULL && tree->left == NULL)
+                return (1);
+
+        return (binary_tree_is_full(tree->right) && binary_tree_is_full(tree->left));
 }
