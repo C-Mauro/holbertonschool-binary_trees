b0672a7a60cfc0b7356913bd1d3bde8fcdc80b03
Fedejalife99 <fedejalife@gmail.com>
2023-01-15 09:55:12 -0800

    first commit task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent = node->parent;
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (parent->parent->left == parent)
+		return (parent->parent->right);
+	else
+		return (parent->parent->left);
+}
