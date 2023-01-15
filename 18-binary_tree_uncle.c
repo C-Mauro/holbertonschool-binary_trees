efa5a01d7719577a3a75489cf08d0be55bed9024
Fedejalife99 <fedejalife@gmail.com>
2023-01-15 10:07:31 -0800

    second commit task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,12 +1,17 @@
 #include "binary_trees.h"
 /**
- *
- *
+ *binary_tree_uncle - function that return the uncle of a node
+ *@node: node to verify
+ *Return: uncle or NULL
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *parent = node->parent;
+
+	if (node == NULL)
+		return (NULL);
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	if (parent->parent->left == parent)
